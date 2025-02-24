75fcfa2f2fb812b9099fdc68ecd7f921c7f058ec
Никита Теслюк <nikiteslyuk@yandex.ru>
2025-02-24 18:21:36 +0300

    доработка разбора и обработки команды addmon + добавление в обработку команды addmon проверки того, что <name> - имя штатного монстра

---
+++ 20250224/2/prog.py
@@ -0,0 +1,51 @@
+import cowsay
+
+
+def encounter(x, y):
+    print(cowsay.cowsay(field[x][y]))
+
+
+gridsize = 10
+field = [["" for _ in range(gridsize)] for _ in range(gridsize)]
+player = [0, 0]
+
+
+while command := input(">> "):
+    comm = command.split()
+    stay = False
+    match comm:
+        case ["up"]:
+            player[1] += 1
+            player[1] %= gridsize
+            print(f"Moved to ({player[0]}, {player[1]})")
+        case ["right"]:
+            player[0] += 1
+            player[0] %= gridsize
+            print(f"Moved to ({player[0]}, {player[1]})")
+        case ["down"]:
+            player[1] -= 1
+            player[1] %= gridsize
+            print(f"Moved to ({player[0]}, {player[1]})")
+        case ["left"]:
+            player[0] -= 1
+            player[0] %= gridsize
+            print(f"Moved to ({player[0]}, {player[1]})")
+        case ["addmon", x, y, name, hello]:
+            if not x.isdigit() or not y.isdigit() or name not in cowsay.list_cows():
+                print("Invalid arguments")
+                continue
+            crds = [int(x), int(y)]
+            print(f"Added monster {name} to ({crds[0]}, {crds[1]}) saying {hello}")
+            if field[crds[1]][crds[0]]:
+                print("Replaced the old monster")
+            field[crds[1]][crds[0]] = hello
+            stay = True
+        case _:
+            print("Invalid command")
+            stay = True
+    if stay:
+        continue
+    if field[player[1]][player[0]]:
+        print("Moved to ...")
+        encounter(player[1], player[0])
+    pass
